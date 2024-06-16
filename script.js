ca042d41e4d9d236ecdbec9b6682728d26cd9713
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-16 17:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let audio = null;
+
+function playAudio(params){
+
+audio = document.getElementById("myAudio");
+audio.src = "./sounds/" + params + ".mp3";
+audio.play();
+}
+}
+
+function stop() {
+audio = document.getElementById(“audioPlayer”);
+audio.pause();
+audio.currentTime = 0;
+}
